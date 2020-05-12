8c41767786020147d8da1828b34ca4f03652872c
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-05-12 20:20:24 +0100

    forgot to add ':comments' resource as a child of the ':articles' resource. But fixed!

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/articles/new'
-  
-  resources :articles
+
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 end
