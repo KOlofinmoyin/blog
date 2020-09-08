67d6fa07fad2ba133182a49571e2db2607c85b84
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-08 18:19:50 +0100

    Resolved AssetNotPrecompiled in development error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :articles
+      resources :comments
+
+      root to: "articles#index"
+    end
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
